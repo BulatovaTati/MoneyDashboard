c8a89ef5a9757c4c68483faddde7d8ce99e4512a
Tati <102695106+BulatovaTati@users.noreply.github.com>
2025-04-20 11:45:27 +0300

    Update HomeTab.jsx

--- src/pages/HomeTab/HomeTab.jsx
+++ src/pages/HomeTab/HomeTab.jsx
@@ -1,27 +1,34 @@
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import useMedia from '../../hooks/useMedia.jsx';
+import { getTransactions } from '../../redux/transactions/operations.js';
+import Balance from '../../components/Balance/Balance.jsx';
+import TransactionsList from '../../components/TransactionsList/TransactionsList.jsx';
+import ButtonAddTransactions from '../../components/ButtonAddTransactions/ButtonAddTransactions.jsx';
+import { getTransactionsCategories } from '../../redux/statistics/operations.js';
 
 const HomeTab = () => {
     const { isMobile } = useMedia();
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
 
-    return <p>Home</p>;
+    // Написати операцію, яка робить запит за транзакціями авторизованого користувача і записує результат у redux store
+    // Написати операцію, яка робить запит за категоріями транзакцій і записує результат у redux store
+
+    // useEffect(() => {
+    //     dispatch(getTransactions());
+    // }, [dispatch]);
+    // useEffect(() => {
+    //     dispatch(getTransactionsCategories());
+    // }, [dispatch]);
+
+    return (
+        <div>
+            HomeTab
+            {isMobile && <Balance />}
+            <TransactionsList />
+            <ButtonAddTransactions />
+        </div>
+    );
 };
 
 export default HomeTab;
-
-
-// import { useDispatch } from 'react-redux';
-// import { useEffect } from 'react';
-// import TransactionsList from '../../components/TransactionsList/TransactionsList'
-// import useMedia from '../../hooks/useMedia'
-// import ButtonAddTransactions from '../../components/ButtonAddTransactions/ButtonAddTransactions'
-// const HomeTab = () => {
-//     const { isMobile } = useMedia();
-//     const dispatch = useDispatch();
-
-//     return (<div><TransactionsList/>
-//         <ButtonAddTransactions/></div>);
-// };
-
-// export default HomeTab;

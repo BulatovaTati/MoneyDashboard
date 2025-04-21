a653dc1408ab4bd63e7c47537ff1c729f57e41b7
Tati <102695106+BulatovaTati@users.noreply.github.com>
2025-04-21 15:18:02 +0300

    Update ModalAddTransaction.jsx

--- src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -1,12 +1,8 @@
-import Modal from 'react-modal';
-
 import { useSelector, useDispatch } from 'react-redux';
 import AddTransactionForm from '../AddTransactionForm/AddTransactionForm';
 import { selectIsAddModalOpen } from '../../redux/modals/selectors';
 import ModalWrapper from '../ModalWrapper/ModalWrapper';
 import css from './ModalAddTransaction.module.css';
-
-Modal.setAppElement('#root');
 
 function ModalAddTransaction() {
     const dispatch = useDispatch();

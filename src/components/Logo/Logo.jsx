55bbe041789506cdcf8aec37eb6325b406283bbd
Tati <102695106+BulatovaTati@users.noreply.github.com>
2025-04-23 20:07:00 +0300

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import s from './Logo.module.css';
 const Logo = ({ type }) => {
     return (
         <div className={clsx(s.logo, type === 'header' && s.headerLogo, type === 'modal' && s.modalLogo)}>
-            <img src="/public/favicon.svg" alt="Money Guard Logo" className={s.logoImage} />
+            <img src="/favicon.svg" alt="Money Guard Logo" className={s.logoImage} />
             <h2 className={s.textLogo}>Money Guard</h2>
         </div>
   );

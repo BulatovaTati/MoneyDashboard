a02ed1347c6613c281fe0526f9e70daec72ac1dd
Lolita <lolita454462@gmail.com>
2025-04-23 15:00:49 +0200

    registration functionality

--- src/validations/validateRegisterForm.js
+++ src/validations/validateRegisterForm.js
@@ -1,4 +1,11 @@
-// Усі поля обов'язкові для заповнення.
-// Email слід перевірити на валідність.
-// Password повинен вміщати від 6 до 12 (включно) символів,
-// а password & confirm password повинні збігатися
+import * as yup from 'yup';
+
+export const registerValidationSchema = yup.object().shape({
+    name: yup.string().required('Name is required').min(2, 'Name must be at least 2 characters').trim(),
+    email: yup.string().email('Invalid email').required('Email is required').min(3, 'Must be at least 3 characters long').max(50, 'Too long').trim(),
+    password: yup.string().required('Password is required').min(8, 'Must be at least 8 characters long').max(12, 'Too long'),
+    confirmPassword: yup
+        .string()
+        .required('Confirm Password is required')
+        .oneOf([yup.ref('password'), null], 'Passwords must match'),
+});

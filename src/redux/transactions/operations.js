2e0fe860d583969eaeabe5b7f2237b1edd090b54
Tati <102695106+BulatovaTati@users.noreply.github.com>
2025-04-24 08:35:30 +0300

    Update operations.js

--- src/redux/transactions/operations.js
+++ src/redux/transactions/operations.js
@@ -24,7 +24,7 @@ export const addTransactions = createAsyncThunk('transactions/add', async (trans
 
 export const deleteTransactions = createAsyncThunk('transactions/delete', async (id, thunkApi) => {
     try {
-        await userTransactionsApi.delete(`/api/transactions/${id}`);
+        await userTransactionsApi.delete(`/api/transactions/${id}`);             thunkAPI.dispatch(getBalanceThunk());
         return id;
     } catch (error) {
         return thunkApi.rejectWithValue(error.message);
@@ -34,6 +34,8 @@ export const deleteTransactions = createAsyncThunk('transactions/delete', async 
 export const editTransactions = createAsyncThunk('transactions/edit', async ({ id, transaction }, thunkApi) => {
     try {
         const { data } = await userTransactionsApi.patch(`/api/transactions/${id}`, transaction);
+      thunkAPI.dispatch(getBalanceThunk());
+      thunkAPI.dispatch(getTransactions());
         return data;
     } catch (error) {
         return thunkApi.rejectWithValue(error.message);

ca1f6f46851e06da69d2769bb092bea74a4637d5
BulatovaTati <kohodatat@gmail.com>
2025-04-24 17:57:51 +0300

    add format code

--- src/components/TransactionsItem/TransactionsItem.jsx
+++ src/components/TransactionsItem/TransactionsItem.jsx
@@ -1,17 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
 import useMedia from '../../hooks/useMedia';
 import { deleteTransactions } from '../../redux/transactions/operations';
-import { openEditModal, addEditId } from '../../redux/modals/slice';
+import { openEditModal } from '../../redux/modals/slice';
 import s from './TransactionsItem.module.css';
 import { selectCategories } from '../../redux/statistics/selectors';
 import { LuPencil } from 'react-icons/lu';
 import { setCurrentTransaction } from '../../redux/transactions/slice';
 
 const getTransactionCategory = (categoryId, categories) => {
-    const transactionCategory = categories.find((item) => item.id === categoryId);
+    const transactionCategory = categories.find(item => item.id === categoryId);
     return transactionCategory ? transactionCategory.name : 'Unknown';
 };
-
 
 const formatDate = dateString => {
     if (!dateString) return 'Invalid date';
@@ -19,10 +18,8 @@ const formatDate = dateString => {
     return new Date(dateString).toLocaleDateString('uk-UA', options);
 };
 
-
 function TransactionsItem({ transaction }) {
     if (!transaction) return null;
-
 
     const sum = Math.abs(transaction.amount);
     const formSum = new Intl.NumberFormat().format(sum);
@@ -32,51 +29,45 @@ function TransactionsItem({ transaction }) {
     const { isMobile } = useMedia();
 
     const onEdit = () => {
-      dispatch(openEditModal());
-      dispatch(setCurrentTransaction({ transaction }));
+        dispatch(openEditModal());
+        dispatch(setCurrentTransaction({ transaction }));
     };
-
 
     const onDelete = async () => {
         dispatch(deleteTransactions(transaction._id));
     };
 
     return !isMobile ? (
-      <tr className={s.tableSection}>
-<td className={s.date}>{formatDate(transaction.date)}</td>
-<td className={s.type}>{transaction.type === 'INCOME' ? '+' : '-'}</td>
-<td className={s.category}>{category}</td>
-<td className={s.comment}>{transaction.comment}</td>
-<td className={transaction.type === 'INCOME' ? s.income : s.expense}>
-  {formSum}
-</td>
-<td className={s.actionBtn}>
-  
-                      <button type="button" className={s.editBtn} onClick={onEdit}>
-                      <LuPencil style={{ width: '14px', height: '14px' }} />
-                      </button>
-                      <button type="button" className={s.deleteBtn} onClick={onDelete}>
-                        Delete
-                      </button>
-</td>
-</tr>
-  ):(
+        <tr className={s.tableSection}>
+            <td className={s.date}>{formatDate(transaction.date)}</td>
+            <td className={s.type}>{transaction.type === 'INCOME' ? '+' : '-'}</td>
+            <td className={s.category}>{category}</td>
+            <td className={s.comment}>{transaction.comment}</td>
+            <td className={transaction.type === 'INCOME' ? s.income : s.expense}>{formSum}</td>
+            <td className={s.actionBtn}>
+                <button type="button" className={s.editBtn} onClick={onEdit}>
+                    <LuPencil style={{ width: '14px', height: '14px' }} />
+                </button>
+                <button type="button" className={s.deleteBtn} onClick={onDelete}>
+                    Delete
+                </button>
+            </td>
+        </tr>
+    ) : (
         <tr className={transaction.type === 'INCOME' ? s.tableSection : s.tableSectionExp}>
             <td className={s.date}>{formatDate(transaction.date)}</td>
             <td className={s.type}>{transaction.type === 'INCOME' ? '+' : '-'}</td>
             <td className={s.category}>{category}</td>
             <td className={s.comment}>{transaction.comment}</td>
-            <td className={transaction.type === 'INCOME' ? s.income : s.expense}>
-                {formSum}
-            </td>
+            <td className={transaction.type === 'INCOME' ? s.income : s.expense}>{formSum}</td>
             <td className={s.actionBtn}>
-                 <button type="button" className={s.deleteBtn} onClick={onDelete}>
+                <button type="button" className={s.deleteBtn} onClick={onDelete}>
                     Delete
                 </button>
-                <button type="button" className={s.editBtn} onClick={onEdit}>Edit
+                <button type="button" className={s.editBtn} onClick={onEdit}>
+                    Edit
                     <LuPencil style={{ width: '14px', height: '14px' }} />
                 </button>
-               
             </td>
         </tr>
     );

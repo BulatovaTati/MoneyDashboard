10e2791a3654fe4a5a694c4d64356f501a51b79e
Yurii <oniks9821@gmail.com>
2025-04-26 18:28:41 +0300

    fix stat

--- src/components/StatisticsDashboard/StatisticsDashboard.jsx
+++ src/components/StatisticsDashboard/StatisticsDashboard.jsx
@@ -1,49 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { getTransactionsCategories, getExpenseSummaryByCategories, getIncomeAndExpenseSummaryByPeriod } from '../../redux/statistics/operations';
-
+import React from 'react';
 import { Listbox, ListboxButton, ListboxOptions, ListboxOption } from '@headlessui/react';
-
 import css from './StatisticsDashboard.module.css';
 
 const months = ['All month', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
 const years = Array.from({ length: new Date().getFullYear() - 2020 + 1 }, (_, i) => `${2020 + i}`);
 
-const StatisticsDashboard = () => {
-    const dispatch = useDispatch();
-
-    const currentMonthIndex = new Date().getMonth() + 1;
-    const currentMonth = months[currentMonthIndex];
-    const currentYear = `${new Date().getFullYear()}`;
-
-    const [selectedMonth, setSelectedMonth] = useState(currentMonth);
-    const [selectedYear, setSelectedYear] = useState(currentYear);
-
-    const [isMonthOpen, setIsMonthOpen] = useState(false);
-    const [isYearOpen, setIsYearOpen] = useState(false);
-
-    useEffect(() => {
-        dispatch(getTransactionsCategories());
-    }, [dispatch]);
-
-    useEffect(() => {
-        const period = selectedMonth === 'All month' ? { year: selectedYear } : { month: selectedMonth, year: selectedYear };
-
-        dispatch(getExpenseSummaryByCategories(period));
-        dispatch(getIncomeAndExpenseSummaryByPeriod(period));
-    }, [dispatch, selectedMonth, selectedYear]);
-
+const StatisticsDashboard = ({ selectedMonth, selectedYear, onMonthChange, onYearChange }) => {
     return (
         <div className={css.wrapper}>
-            {/* Month Dropdown */}
-            <div className={`${css.dropdownWrapper} ${isMonthOpen ? css.open : ''}`}>
-                <Listbox value={selectedMonth} onChange={setSelectedMonth}>
+            {/* Month */}
+            <div className={css.dropdownWrapper}>
+                <Listbox value={selectedMonth} onChange={onMonthChange}>
                     <div>
-                        <ListboxButton className={css.dropdownButton} onClick={() => setIsMonthOpen(prev => !prev)}>
-                            {selectedMonth}
-                        </ListboxButton>
-                        <ListboxOptions className={css.dropdownList} onBlur={() => setIsMonthOpen(false)}>
+                        <ListboxButton className={css.dropdownButton}>{selectedMonth}</ListboxButton>
+                        <ListboxOptions className={css.dropdownList}>
                             {months.map(month => (
                                 <ListboxOption key={month} value={month} as="li">
                                     {({ selected }) => <div className={`${css.dropdownItem} ${selected ? css.dropdownItemActive : ''}`}>{month}</div>}
@@ -54,14 +24,12 @@ const StatisticsDashboard = () => {
                 </Listbox>
             </div>
 
-            {/* Year Dropdown */}
-            <div className={`${css.dropdownWrapper} ${isYearOpen ? css.open : ''}`}>
-                <Listbox value={selectedYear} onChange={setSelectedYear}>
+            {/* Year */}
+            <div className={css.dropdownWrapper}>
+                <Listbox value={selectedYear} onChange={onYearChange}>
                     <div>
-                        <ListboxButton className={css.dropdownButton} onClick={() => setIsYearOpen(prev => !prev)}>
-                            {selectedYear}
-                        </ListboxButton>
-                        <ListboxOptions className={css.dropdownList} onBlur={() => setIsYearOpen(false)}>
+                        <ListboxButton className={css.dropdownButton}>{selectedYear}</ListboxButton>
+                        <ListboxOptions className={css.dropdownList}>
                             {years.map(year => (
                                 <ListboxOption key={year} value={year} as="li">
                                     {({ selected }) => <div className={`${css.dropdownItem} ${selected ? css.dropdownItemActive : ''}`}>{year}</div>}

903fff53798e7d4e175f53bf1f34ceb0aa5de65f
BulatovaTati <kohodatat@gmail.com>
2025-04-26 10:29:35 +0300

    add redux corrections

--- src/pages/HomeTab/HomeTab.jsx
+++ src/pages/HomeTab/HomeTab.jsx
@@ -1,23 +1,15 @@
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
 import useMedia from '../../hooks/useMedia';
 import Balance from '../../components/Balance/Balance';
 import TransactionsList from '../../components/TransactionsList/TransactionsList';
 import ButtonAddTransactions from '../../components/ButtonAddTransactions/ButtonAddTransactions';
-import { getTransactions } from '../../redux/transactions/operations';
 
-import css from './HomeTab.module.css';
+import s from './HomeTab.module.css';
 
 const HomeTab = () => {
     const { isMobile } = useMedia();
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getTransactions());
-    }, [dispatch]);
 
     return (
-        <div className={css.homeTab}>
+        <div className={s.homeTab}>
             {isMobile && <Balance />}
             <TransactionsList />
             <ButtonAddTransactions />

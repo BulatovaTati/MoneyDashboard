bdb2686ae6b4b0ac4c2f49019f0a52a240f92803
BulatovaTati <kohodatat@gmail.com>
2025-04-27 20:42:16 +0300

    add logo corrections

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,9 @@
 import clsx from 'clsx';
 import s from './Logo.module.css';
 
-const Logo = ({ type, imageClassName, textClassName }) => {
+const Logo = ({ imageClassName, textClassName }) => {
     return (
-        <div className={clsx(s.logo, type === 'header' && s.headerLogo, type === 'modal' && s.modalLogo)}>
+        <div className={clsx(s.logo)}>
             <img src="/favicon.svg" alt="Money Guard Logo" className={clsx(s.logoImage, imageClassName)} />
             <h2 className={clsx(s.textLogo, textClassName)}>Money Guard</h2>
         </div>

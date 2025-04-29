a27abd7cd0ecaac3cffd4b8d095354c3c489213a
BulatovaTati <kohodatat@gmail.com>
2025-04-29 17:36:15 +0300

    change Balance comp

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,28 +1,15 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { getBalanceThunk } from '../../redux/auth/operations';
+import { useSelector } from 'react-redux';
 import { selectUserBalance } from '../../redux/auth/selectors';
+import { formattedBalance } from '../../helpers/formattedBalance';
 import s from './Balance.module.css';
 
 const Balance = () => {
     const balance = useSelector(selectUserBalance);
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getBalanceThunk());
-    }, [dispatch]);
-
-    const formattedBalance = new Intl.NumberFormat('uk-UA', {
-        style: 'decimal',
-        minimumFractionDigits: 2,
-    })
-        .format(balance)
-        .replace(',', '.');
 
     return (
         <div className={s.balanceWrapper}>
             <p className={s.text}> Your balance</p>
-            <p className={s.amount}>₴ {formattedBalance}</p>
+            <p className={s.amount}>₴ {formattedBalance(balance)}</p>
         </div>
     );
 };
